0ad9618b6f16005875db7173fb5ffafc32e9aa9d
tuneman7 <tuneman7@berkeley.edu>
2023-11-10 06:10:54 -0800

    periodic update

--- dashboard/customer_products.py
+++ dashboard/customer_products.py
@@ -1,7 +1,9 @@
 from includes import *
 from flask import Blueprint
 from libraries.database_aggregator import database_aggregator as da
-from libraries.altair_rendering import AltairRendering
+from libraries.altair_rendering import AltairRendering as ar
+from libraries.utility import Utility as mutil
+import pandas as pd
 import os
 
 customer_products = Blueprint('customer_products', __name__)
@@ -15,16 +17,38 @@ def consume_fastapi_start():
         section_number="five"))
         return res
 
-def dataframe_to_dict(dataframe):
-    print(jsonify({'data': dataframe.to_dict(orient='records')}))
-    return jsonify({'data': dataframe.to_dict(orient='records')})
+import glob
+import os
+def clear_json_files():
+        mu = mutil()
+        pattern = 'altair-data*.json'
+        # Get a list of matching files in the directory
+        matching_files = glob.glob(os.path.join(mu.get_this_dir(), pattern))
 
+        
+        if matching_files:
+                for file_path in matching_files:
+                        # Loop through the matching files and delete them
+                        for file_path in matching_files:
+                                os.remove(file_path)
+
+         
 @customer_products.route('/render_customer_products', methods=['POST', 'GET'])
 def render_customer_data_modal(popout = False):
 
         chart_json=None
         total_slides = 1
         slide_no=1
+
+        mar = ar()
+        mda = da()
+        
+        chart_json = mar.get_cust_summary_images(mda.get_customer_product_data()).configure_axis(
+                grid=False
+            ).configure_view(
+                strokeWidth=0
+            ).to_json()            
+
 
         res = jsonify({'htmlresponse':render_template('modal/customer_data.modal.html',titles=[''],
                                                       slide_no=slide_no,total_slides=total_slides,chart_json=chart_json)})
@@ -35,6 +59,8 @@ def render_customer_data_modal(popout = False):
 def data():
         mda = da()
         df = mda.get_customer_product_data()
+        df['post_date'] = pd.to_datetime(df['post_date'])
+        df['post_date'] = df['post_date'].dt.strftime('%Y-%m-%d %H:%M')
         data_list = df.to_dict(orient="records")
         #print(data_list)
         return jsonify({'data': data_list})
--- dashboard/libraries/altair_rendering.py
+++ dashboard/libraries/altair_rendering.py
@@ -3,7 +3,6 @@ from libraries.utility import Utility
 import numpy as np
 
 import altair as alt
-from vega_datasets import data
 import os
 import pandas as pd
 import pandasql as psql
@@ -19,3 +18,79 @@ class AltairRendering:
         else:
             self.my_data_object = database_aggregator(load_data_from_url=load_data_from_url)
 
+
+    def get_cust_summary_images(self,df):
+        df["amt_usd"] = df["amt_usd"].replace('[\$,]', '', regex=True).astype(float)
+        data = df
+
+        # Assuming 'data' is your DataFrame after cleaning
+        data['post_date'] = pd.to_datetime(data['post_date'])
+        data['amt_usd'] = pd.to_numeric(data['amt_usd'])
+
+        # Group the data by month and calculate the sum of 'amt_usd' in each month
+        monthly_revenue = data.groupby(data['post_date'].dt.to_period("M")).sum()['amt_usd'].reset_index()
+
+        # Convert the Period object to a string for 'post_date' column
+        monthly_revenue['post_date'] = monthly_revenue['post_date'].dt.strftime('%Y-%m')
+
+
+        # Altair Visualizations
+        # 1. Monthly Revenue Over Time
+        line = alt.Chart(monthly_revenue).mark_line().encode(
+            x='post_date:T',
+            y='amt_usd:Q'
+        )
+
+        points = alt.Chart(monthly_revenue).mark_point(size=150, opacity=0).encode(
+            x='post_date:T',
+            y='amt_usd:Q',
+            tooltip=[alt.Tooltip('post_date:T', title='Date'), alt.Tooltip('amt_usd:Q', title='Revenue', format='$,.2f')]
+        )
+
+        monthly_revenue_chart = line + points
+        monthly_revenue_chart = monthly_revenue_chart.properties(
+            title='Monthly Revenue Over Time',
+            width=300,
+            height=150
+        )
+        # 2. Distribution of Transaction Amounts with Sine Wave
+        transaction_distribution_chart = alt.Chart(data).mark_bar(color='#bdd7e7').encode(
+            alt.X("amt_usd:Q", bin=alt.Bin(maxbins=30), title='Transaction Amount (USD)'),
+            y='count()',
+            tooltip=[alt.Tooltip('count()', title='Number of Transactions'), alt.Tooltip('amt_usd:Q', title='Amount', format='$,.2f')]
+        ).properties(
+            title='Distribution of Transaction Amounts',
+            width=300,
+            height=150
+        )  # Combine the sine wave chart with the transaction distribution chart
+
+        # Group the data by 'post_date' and 'product_type' and calculate the sum of 'amt_usd' in each group
+        revenue_product_type = data.groupby(['post_date', 'product_type']).sum()['amt_usd'].reset_index()
+
+        # Convert the 'post_date' column to a string for better visualization
+        revenue_product_type['post_date'] = revenue_product_type['post_date'].dt.strftime('%Y-%m')
+
+
+        # 3. Revenue by Product Type
+        revenue_product_chart = alt.Chart(revenue_product_type).mark_bar(color='#bdd7e7').encode(
+            x='amt_usd:Q',
+            y=alt.Y('product_type:N', sort='-x'),
+            tooltip=[alt.Tooltip('product_type:N', title='Product Type'), alt.Tooltip('amt_usd:Q', title='Revenue', format='$,.2f')]
+        ).properties(
+            title='Revenue by Product Type',
+            width=300,
+            height=150
+        )
+
+        # Combine charts into a single visualization
+        combined_chart = alt.hconcat(monthly_revenue_chart, transaction_distribution_chart, revenue_product_chart).resolve_scale(
+            color='independent'
+        )
+
+        #alt.data_transformers.enable('json')
+        #alt.data_transformers.enable("vegafusion")
+        alt.data_transformers.disable_max_rows()
+
+
+        # Display the chart
+        return combined_chart
